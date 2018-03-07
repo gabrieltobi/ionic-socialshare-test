1eadd29d4c2585c58a292f22e801a0a27c49d06a
Gabriel Tubias <gabrieltubiass@gmail.com>
2018-03-07 07:38:04 -0300

    More test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,18 +7,50 @@ import { SocialSharing } from '@ionic-native/social-sharing';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  canShareTwitter: any;
+  canShareInstagram: any;
+
   constructor(public navCtrl: NavController, private socialSharing: SocialSharing) {
+    /* this.socialSharing.canShareVia('twitter')
+      .then(result => this.canShareTwitter = result);
+
+    this.socialSharing.canShareVia('instagram')
+      .then(result => this.canShareInstagram = result); */
   }
 
   share() {
-    this.socialSharing.shareWithOptions({ message: 'Message', subject: 'Subject' });
+    this.socialSharing.shareWithOptions({ message: 'Message', subject: 'Subject' })
+      .catch(err => {
+        console.log(err);
+        if (typeof err !== 'string') {
+          alert(JSON.stringify(err));
+        } else {
+          alert(err);
+        }
+      });
   }
 
   shareTwitter() {
-    this.socialSharing.shareViaTwitter('Message');
+    this.socialSharing.shareViaTwitter('Message')
+      .catch(err => {
+        console.log(err);
+        if (typeof err !== 'string') {
+          alert(JSON.stringify(err));
+        } else {
+          alert(err);
+        }
+      });
   }
 
   shareInstagram() {
-    this.socialSharing.shareViaInstagram('Message', 'http://via.placeholder.com/350x150');
+    this.socialSharing.shareViaInstagram('Message', 'http://via.placeholder.com/350x150')
+      .catch(err => {
+        console.log(err);
+        if (typeof err !== 'string') {
+          alert(JSON.stringify(err));
+        } else {
+          alert(err);
+        }
+      });
   }
 }
